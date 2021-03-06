4a97646f238cb1b4fb5f0f5c3598a9718f2acd4b
Willie <ingverger@gmail.com>
2021-03-06 19:56:57 -0300

    Ajustes para Appflow de Ionic

---
+++ www/version.js
@@ -0,0 +1,15 @@
+/*
+	version.js
+	18/3/2020
+	Para llevar centralizado el numero de versión
+
+*/
+
+const 
+  versionString="0.5.0" //  lleva el numero de version actual
+
+
+	function version(){
+		return 'Versión ' + versionString;
+	};
+
